559abc3e603afe81b02fa77ea27878b51c526e79
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-17 15:30:17 +0530

    Update url.js

--- controllers/url.js
+++ controllers/url.js
@@ -1,23 +1,15 @@
 import URL from "../models/url.js";
 import { nanoid } from "nanoid";
 
-export const shortURL = async (req, res) => {
+const shortURL = async (req, res) => {
   const shortID = nanoid(8);
 
   await URL.create({
     urlId: shortID,
     origURL: req.body.url,
-    visitedHistory: [],
   });
 
   res.render("home.ejs", { id: shortID });
 };
 
-export const getAnalytics = async (req, res) => {
-  const shortId = req.params.urlId;
-  const ans = await URL.findOne({ shortId });
-  return res.json({
-    totalClicks: ans.visitedHistory.length,
-    analytics: ans.visitedHistory,
-  });
-};
+export default shortURL;
