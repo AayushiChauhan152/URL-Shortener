ac6a966e460263341d8a9d5b152b88522b156962
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-17 18:35:48 +0530

    Update url.js

--- models/url.js
+++ models/url.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const urlSchema = new mongoose.Schema(
-  {
-    urlId: {
-      type: String,
-      required: true
-    },
-    origURL: {
-      type: String,
-    },
-);
+const urlSchema = new mongoose.Schema({
+  urlId: {
+    type: String,
+    unique: true,
+  },
+  origURL: {
+    type: String,
+  },
+});
 const url = new mongoose.model("url", urlSchema);
 export default url;
