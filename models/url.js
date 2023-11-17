08d5f16d63310358c1dc6729444ed913cbdaa089
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-17 15:28:45 +0530

    Update url.js

--- models/url.js
+++ models/url.js
@@ -4,13 +4,11 @@ const urlSchema = new mongoose.Schema(
   {
     urlId: {
       type: String,
-      unique: true,
+      required: true
     },
     origURL: {
       type: String,
     },
-    visitedHistory: [{ timestamp: { type: Number } }],
-  },
   { timestamps: true }
 );
 const url = new mongoose.model("url", urlSchema);
