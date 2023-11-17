cdeea1eb57b50c7db8daa6731fb2da0f4d50997d
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-17 15:27:16 +0530

    Update staticRoutes.js

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -1,12 +1,8 @@
 import express from "express";
 const router = express.Router();
-import URL from "../models/url.js";
 
 router.get("/", async (req, res) => {
-  const urls = await URL.find({});
-  res.render("home.ejs", {
-    urls: urls,
-  });
+  res.render("home.ejs");
 });
 
 export default router;
