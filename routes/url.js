cb054efdb500c381e97e7c10f0d5d1981defe1c5
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-17 15:26:18 +0530

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { shortURL, getAnalytics } from "../controllers/url.js";
+import  shortURL from "../controllers/url.js";
 
 router.post("/", shortURL);
-router.get("/analytics/:shortId", getAnalytics);
-
 export default router;
