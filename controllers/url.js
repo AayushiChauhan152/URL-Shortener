0b66122db2e0a06a3c2fefc246c35a3cce854873
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2023-11-18 15:05:16 +0530

    Update url.js

--- controllers/url.js
+++ controllers/url.js
@@ -2,14 +2,21 @@ import URL from "../models/url.js";
 import { nanoid } from "nanoid";
 
 const shortURL = async (req, res) => {
-  const shortID = nanoid(8);
+  const url = req.body.url;
 
-  await URL.create({
-    urlId: shortID,
-    origURL: req.body.url,
-  });
+  const ifExists = await URL.findOne({ origURL: url });
 
-  res.render("home.ejs", { id: shortID });
+  if (ifExists) {
+    return res.render("home.ejs", { id: ifExists.urlId });
+  } else {
+    const shortID = nanoid(8);
+
+    await URL.create({
+      urlId: shortID,
+      origURL: req.body.url,
+    });
+    res.render("home.ejs", { id: shortID });
+  }
 };
 
 export default shortURL;
